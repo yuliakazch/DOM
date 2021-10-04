bcc9f8a0090abb2cb462c35302e074a557b22132
Yulia Kazachok <yuliakazachok@gmail.com>
2021-10-04 20:37:50 +0700

    Create project

---
+++ app/src/main/java/com/dom/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.dom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.dom.ui.theme.DOMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DOMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DOMTheme {
+        Greeting("Android")
+    }
+}
